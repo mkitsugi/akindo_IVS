19952d4c4099ce1cb6a60b324f9d21a4767370ef
ryouhei0622 <seijakunomutou@gmail.com>
2023-06-18 18:59:40 +0900

    データをエンべディング&保存

--- azure/TEST/llama_index_QA.py
+++ azure/TEST/llama_index_QA.py
@@ -2,14 +2,76 @@ import os
 
 import openai
 from dotenv import load_dotenv
-from llama_index import Document, LangchainEmbedding, ServiceContext, VectorStoreIndex
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts.chat import (
+    AIMessagePromptTemplate,
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from llama_index import (
+    Document,
+    LangchainEmbedding,
+    LLMPredictor,
+    QuestionAnswerPrompt,
+    ServiceContext,
+    VectorStoreIndex,
+)
+from llama_index.prompts.chat_prompts import (
+    CHAT_REFINE_PROMPT,
+    CHAT_REFINE_PROMPT_TMPL_MSGS,
+)
+from llama_index.prompts.default_prompts import DEFAULT_TEXT_QA_PROMPT_TMPL
+from llama_index.prompts.prompts import RefinePrompt
+
+# デフォルトのプロンプトを変更
+QA_PROMPT_TMPL = (
+    "以下の情報を参照してください。 \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "この情報を使って、次の質問に回答してください。: {query_str}\n"
+)
+
+CHAT_REFINE_PROMPT_TMPL_MSGS = [
+    HumanMessagePromptTemplate.from_template("{query_str}"),
+    AIMessagePromptTemplate.from_template("{existing_answer}"),
+    HumanMessagePromptTemplate.from_template(
+        """
+    以下の情報を参照してください。 \n"
+    "---------------------\n"
+    "{context_msg}"
+    "\n---------------------\n"
+    この情報が回答の改善に役立つようならこの情報を使って回答を改善してください。
+    この情報が回答の改善に役立たなければ元の回答を日本語で返してください。
+    """
+    ),
+]
+
+CHAT_REFINE_PROMPT_LC = ChatPromptTemplate.from_messages(CHAT_REFINE_PROMPT_TMPL_MSGS)
+
+QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
+CHAT_PROMPT = RefinePrompt.from_langchain_prompt(CHAT_REFINE_PROMPT_LC)
+
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+# デフォルトと同じだが一応明示する
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
+llm_predictor = LLMPredictor(llm)
+llama_embed = LangchainEmbedding(
+    embedding,
+    embed_batch_size=1,
+)
 # エンベディングモデルをロードする
 # この例では、デフォルトのエンベディングモデルを使用しています
-service_context = ServiceContext.from_defaults()
+service_context = ServiceContext.from_defaults(
+    llm_predictor=llm_predictor,
+    embed_model=llama_embed,
+)
+
 
 # データの準備
 data = [
@@ -25,17 +87,21 @@ data = [
     "僕の趣味覚えてる？",
     "僕のの好きなものは唐揚げなんだー",
 ]
-
+# List[Document]に変形する
 documents = [Document(text=item) for item in data]
 
 # インデックスを作成する
 index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+index.storage_context.persist("azure/TEST/strage")
 
 # クエリエンジンを生成する
-query_engine = index.as_query_engine()
+query_engine = index.as_query_engine(
+    service_context=service_context, text_qa_template=QA_PROMPT, refine_template=CHAT_PROMPT
+)
 
 # クエリを投げる
-qestion = "僕の好きな食べ物は？"
+qestion = "好きな食べ物は？"
 response = query_engine.query(qestion)
 print(response)
 # >>> 唐揚げです。
+print(response.get_formatted_sources(length=4096))
