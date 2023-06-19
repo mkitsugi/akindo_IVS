e1b7334e28c7ba6f3f8758407dd6f20e3a5262df
mkitsugi <mkitsugi250@gmail.com>
2023-06-19 21:28:27 +0900

    chat画面をdemo用に変更/azure functionも動くとこだけ

--- src/pages/api/chat_gptResponse.ts
+++ src/pages/api/chat_gptResponse.ts
@@ -2,14 +2,23 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { message, roomId } = req.body;
+  const { message, user_id, roomId } = req.body;
 
+  console.log(req.body)
   // AZURE_FUNCTION_URLが設定されていない場合にエラーをスロー
   if (!process.env.AZURE_FUNCTION_URL) {
     throw new Error('AZURE_FUNCTION_URL is not set');
   }
 
-  const response = await axios.post(process.env.AZURE_FUNCTION_URL, { message, roomId });
-  const reply = response.data;
-  res.json(reply);
+  try {
+    const response = await axios.post(process.env.AZURE_FUNCTION_URL, { message, roomId });
+    const reply = response.data;
+    res.json(reply);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred when making the request to the Azure Function' });
+  }
+  
+
+  
 };
